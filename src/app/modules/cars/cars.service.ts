6208c9fb079d8f29aee7163082bdcb576e5f7f72
Hridoy Islam <hridoy4t@gmail.com>
2024-04-26 09:53:59 +0600

    user populated

--- src/app/modules/cars/cars.service.ts
+++ src/app/modules/cars/cars.service.ts
@@ -4,7 +4,7 @@ import { TCars } from "./cars.interface";
 import { carSearchableFields } from "./car.constant";
 
 const getAllCarsFromDB = async (query: Record<string, unknown>) => {
-  const carsQuery = new QueryBuilder(Cars.find(), query)
+  const carsQuery = new QueryBuilder(Cars.find().populate("userid"), query)
     .search(carSearchableFields)
     .filter()
     .sort()
